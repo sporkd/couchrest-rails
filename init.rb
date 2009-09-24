1084b7bc6f9f9f387270797bbfe227744fd96a54
Peter Gumeson <peter.gumeson@stemgent.com>
2009-09-23 18:37:52 -0700

    Use CouchRest Validation instead of Validatable

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 require 'couch_rest_rails'
 require 'spec/rails/matchers/couch_document_validations'
 
-%w(couchrest json validatable).each do |g|
+%w(couchrest json).each do |g|
   begin
     require g
   rescue LoadError
